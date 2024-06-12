8a191da461e6326db10623ef4d482b05bd525242
Farhad2590 <farhadhossen2590@gmail.com>
2024-06-13 00:49:42 +0600

    all set up done home banner sigin sign up page updated

---
+++ src/Components/Shared/Navbar/Navbars.jsx
@@ -0,0 +1,83 @@
+
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../../assets/Logo.png"
+import useAuth from "../../../hooks/useAuth";
+import { MdOutlineNotificationAdd } from "react-icons/md";
+
+
+
+
+const Navbars = () => {
+    const { user, logOut } = useAuth()
+    const Navs =
+        <div className="space-x-5">
+            <NavLink to={'/'} className={({ isActive }) => isActive ? 'btn btn-outline border border-orange-400 text-orange-400  hover:outline-none' : ' bg-orange-400 text-white'}>Home</NavLink>
+            <NavLink to={'/'} className={({ isActive }) => isActive ? 'btn btn-outline border border-orange-400 text-orange-400  hover:outline-none' : ' bg-orange-400 text-white'}>Meals</NavLink>
+            <NavLink to={'/'} className={({ isActive }) => isActive ? 'btn btn-outline border border-orange-400 text-orange-400  hover:outline-none' : ' bg-orange-400 text-white'}>Upcomming</NavLink>
+        </div>
+
+    return (
+        <div className="navbar fixed w-full bg-white z-10 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3  z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {Navs}
+                    </ul>
+                </div>
+                <Link to='/'>
+                    <img
+                        className='w-64'
+                        src={logo}
+                        alt='logo'
+                    />
+                </Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {Navs}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <MdOutlineNotificationAdd className="text-3xl mr-2 text-orange-400" />
+                {
+                    user ?
+                        <div className="dropdown dropdown-end">
+
+                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img alt="Tailwind CSS Navbar component" src={user && user.photoURL} />
+                                </div>
+                            </div>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                <li>
+                                    <a className="justify-between">
+                                        {
+                                            user && user.displayName
+                                        }
+                                    </a>
+                                </li>
+                                <li><a>Dashboard</a></li>
+                                <li><button onClick={logOut}>Logout</button></li>
+                            </ul>
+                        </div>
+
+                        :
+                        <Link to="/login">
+                            <button className="btn bg-orange-400  text-white hover:border border-orange-400">
+                                Join Us
+                            </button>
+
+                        </Link>
+
+                }
+            </div>
+        </div>
+
+    );
+};
+
+export default Navbars;
+
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Main from '../Layouts/Main'
+import Home from '../pages/Home/Home'
+import ErrorPage from '../pages/Error/ErrorPage'
+import Login from '../pages/Login/Login'
+import SignUp from '../pages/Signup/SignUp'
+// import RoomDetails from '../pages/RoomDetails/RoomDetails'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      // {
+      //   path: '/room/:id',
+      //   element: <RoomDetails />,
+      // },
+    ],
+  },
+  { path: '/login', element: <Login /> },
+  { path: '/signup', element: <SignUp /> },
+])
